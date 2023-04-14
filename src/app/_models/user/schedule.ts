4def40625afb9a810abe1b428a02146a678e828a
nuttawat <nuttawat7171@gmail.com>
2023-04-14 19:14:35 +0700

    move folder

--- src/app/_models/user/schedule.ts
+++
@@ -1,9 +0,0 @@
-export interface ISchedule {
-  name: string;
-  illness: string;
-  dentist: string;
-  congenitalDisease?: string;
-  drugAllergy?: string;
-  time: Date | string;
-  status: string;
-}
